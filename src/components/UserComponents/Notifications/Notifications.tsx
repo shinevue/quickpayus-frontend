895b8e67075b0361f411aca2047fc18947dea465
shinevue <whinshine401020@gmail.com>
2024-08-09 01:00:04 -0400

    fix: add readall notification API

--- src/components/UserComponents/Notifications/Notifications.tsx
+++ src/components/UserComponents/Notifications/Notifications.tsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
 
 // antd
-import { Layout, Skeleton, Empty, theme } from "antd";
+import { Layout, Empty } from "antd";
 
 // styled components
 import * as Styled from "./Notifications.styled";
@@ -21,7 +21,6 @@ import {
   readOne,
   deleteOne,
 } from "@/app/slices/notificationsSlice";
-import { useUpdateNotificationMutation } from "@/app/slice";
 
 // components
 import PageTitle from "../PageTitle";
@@ -48,13 +47,9 @@ export const Notifications = () => {
     }
   }, [dispatch, location.pathname]);
 
-  const [putData] = useUpdateNotificationMutation();
-
   const handlePutData = useCallback(async () => {
     try {
-      const result = await putData({
-        data: { isRead: true },
-      });
+      const result = await API.put("/notifications/");
       if ("data" in result && result.data.success) {
         // refetchTotal();
         dispatch(setNotificationsReadStatus());
@@ -62,7 +57,7 @@ export const Notifications = () => {
     } catch (error) {
       console.log("error", error);
     }
-  }, [dispatch, putData]);
+  }, [dispatch]);
 
   const handleDeleteData = useCallback(async () => {
     try {
@@ -73,7 +68,7 @@ export const Notifications = () => {
     } catch (error) {
       console.log("error", error);
     }
-  }, [dispatch, putData]);
+  }, [dispatch]);
 
   const handleRead = async (id) => {
     try {
